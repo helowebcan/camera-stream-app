df1ca927747563c07002b5ddde3b9eb0a3622092
helowebcan <heloisasanabiosilva@gmail.com>
2025-12-23 17:17:45 -0300

    Migrate MainActivity from Kotlin to Java
    Converted MainActivity from Kotlin to Java and updated package name.

--- app/src/main/java/com/seuprojeto/MainActivity.kt
+++ app/src/main/java/com/seuprojeto/MainActivity.kt
@@ -1,35 +1,13 @@
-package com.seuprojeto
+package com.example.cameraapp;
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.core.content.ContextCompat
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Cria a visualização da câmera
-        val previewView = PreviewView(this)
-        setContentView(previewView)
-
-        // Inicializa o provedor da câmera
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-
-            // Configura o preview
-            val preview = androidx.camera.core.Preview.Builder().build()
-            preview.setSurfaceProvider(previewView.surfaceProvider)
-
-            // Seleciona a câmera traseira
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            // Liga o ciclo de vida da câmera à Activity
-            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
-        }, ContextCompat.getMainExecutor(this))
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
 
